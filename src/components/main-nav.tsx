1bbe709068107b18643ac1b398ab3a90f211220a
Morteza <mortazapor3@gmail.com>
2024-08-01 21:38:39 +0330

    fix: more spacing on main nav items

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -36,15 +36,12 @@ export default function MainNav() {
       <div className="bg-foreground shadow-md shadow-gray-600 p-4 sm:hidden">
         <Bars3Icon onClick={() => setIsOpen(o => !o)} className="size-8 stroke-background cursor-pointer" />
       </div>
-      <ul className={`${isActive ? 'sm:bg-foreground sm:text-background shadow-md shadow-gray-600' : 'sm:text-foreground sm:bg-transparent'} flex flex-col sm:flex-row gap-6 uppercase bg-foreground origin-top-left [transition:transform_500ms,border-radius_900ms,background-color_200ms] text-background pl-8 pb-8 pt-2 sm:pt-8 sm:justify-center sm:pl-0 sm:[transition:transform_500ms,border-radius_900ms,background-color_0s] sm:gap-9 ${isOpen ? 'scale-1 rounded-none' : 'scale-0 [border-bottom-right-radius:30rem]'}`}>
+      <ul className={`${isActive ? 'sm:bg-foreground sm:text-background shadow-md shadow-gray-600' : 'sm:text-foreground sm:bg-transparent'} flex flex-col sm:flex-row gap-7 uppercase bg-foreground origin-top-left [transition:transform_500ms,border-radius_900ms,background-color_200ms] text-background pl-8 pb-8 pt-3 sm:pt-8 sm:justify-center sm:pl-0 sm:[transition:transform_500ms,border-radius_900ms,background-color_0s] sm:gap-9 ${isOpen ? 'scale-1 rounded-none' : 'scale-0 [border-bottom-right-radius:30rem]'}`}>
         <li className={`transition-[opacity] delay-300 ${isOpen ? 'opacity-1' : 'opacity-0'}`}>
           <Link href="#header" className="nav-link">Home</Link>
         </li>
         <li className={`transition-[opacity] [transition-delay:340ms] ${isOpen ? 'opacity-1' : 'opacity-0'}`}>
           <Link href="#projects" className="nav-link">Projects</Link>
-        </li>
-        <li className={`transition-[opacity] [transition-delay:380ms] ${isOpen ? 'opacity-1' : 'opacity-0'}`}>
-          <Link href="#resume" className="nav-link">Resume</Link>
         </li>
         <li className={`transition-[opacity] [transition-delay:420ms] ${isOpen ? 'opacity-1' : 'opacity-0'}`}>
           <Link href="#about" className="nav-link">About</Link>
