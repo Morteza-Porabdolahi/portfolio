33672a3729aa98447a94d87e7c5e21318e0627b2
Morteza <74528563+Morteza-Porabdolahi@users.noreply.github.com>
2024-08-10 04:31:08 +0000

    feature: using <Image /> component in header for optimizations

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,20 @@
 import MainNav from "@/components/main-nav";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function Header() {
   const t = useTranslations("Header.Message");
   return (
     <header
-      className="flex min-h-dvh flex-col items-center justify-center gap-8 bg-[url('/morteza.jpg')] bg-cover bg-center bg-no-repeat px-5"
+      className="relative flex min-h-dvh flex-col items-center justify-center gap-8 px-5"
       id="header"
     >
+      <Image
+        src="/morteza.jpg"
+        alt="Morteza's face"
+        fill
+        className="-z-10 object-cover"
+      />
       <MainNav />
       <h1 className="flex flex-col items-center gap-2 text-center text-4xl font-thin lg:text-6xl">
         {t.rich("message", {
