ef4c3ed68a646b3999f44c0bc994436965c3cd5f
Morteza <74528563+Morteza-Porabdolahi@users.noreply.github.com>
2024-08-12 12:26:05 +0000

    feature: delete loading.ts file (unnecessary)

--- src/app/[lang]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import Spinner from "@/components/ui/Spinner";
-
-export default function Loading() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-primary">
-      <Spinner />
-    </div>
-  );
-}
