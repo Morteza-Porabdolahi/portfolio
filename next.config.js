680d69f1583ef1edb188bd827bc805a71f4d13f0
Morteza <74528563+Morteza-Porabdolahi@users.noreply.github.com>
2024-08-12 16:30:43 +0000

    feature: setting up pwa

--- next.config.js
+++ next.config.js
@@ -1,28 +1,36 @@
-import createNextIntlPlugin from 'next-intl/plugin'
+import createNextIntlPlugin from "next-intl/plugin";
+import withSerwistInit from "@serwist/next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.js");
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const config = {
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'utfs.io',
-        pathname: '/**'
-      }
-    ]
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+    ],
   },
   typescript: {
-    ignoreBuildErrors: true
-  }, 
+    ignoreBuildErrors: true,
+  },
   eslint: {
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default withNextIntl(config);
+const withSerwist = withSerwistInit({
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist(withNextIntl(config));
--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -8,12 +8,37 @@ import { Toaster } from "@/components/ui/sooner";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 
+const APP_NAME = "Morteza Porabdolahi's Portfolio";
+const APP_DEFAULT_TITLE = "Morteza Porabdolahi";
+const APP_DESCRIPTION =
+  "Front-end developer with +3 years experience in building different websites";
+
 export const metadata: Metadata = {
-  title: "Morteza Porabdolahi",
-  description:
-    "Front-end developer with +3 years experience in building different websites",
+  applicationName: APP_NAME,
+  title: APP_DEFAULT_TITLE,
+  description: APP_DESCRIPTION,
   keywords: "portfolio, developer, front-end, Javascript, React",
   icons: [{ rel: "icon", url: "/favicon.png" }],
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+  },
 };
 
 const vazir = Vazirmatn({ display: "swap", subsets: ["arabic"] });
